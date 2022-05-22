70cb0620a227f1632d6300f657e44d3c54c2abb5
Donovan <donniemassenburg75@gmail.com>
2022-05-22 18:50:29 -0400

    final commit

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 this program displays the current dates buisness hours and allows the user to edit and save task to a specific block with colored times for past, current and future times.
 
+https://atlas075.github.io/calender/
 
-image.png
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,95 @@
-let currentDate = new Date();
-let day = currentDate.getDay();
-let month = currentDate.getMonth();
-let year = currentDate.getFullYear();
-let numberDate = currentDate.getDate();
-// let time = currentDate.getTime();
-var dayList = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Firday", "Saturday"];
-var monthList = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-var dateList = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", "13th", "14th", "15th", "16th", "17th", "18th", "19th", "20th", "21st", "22nd", "23rd", "24th", "25th", "26th", "27th", "28th", "29th", "30th", "31st"]
-document.querySelector(".day").innerHTML = "Todays is " + dayList[day] + " " + monthList[month] + " " + dateList[numberDate -1] + ".";
+// displays the current day in (mm/dd)
+var currentDay = document.querySelector('currentDay')
+document.querySelector(".day").innerHTML = moment().format("dddd MMM Do")
 var time = moment().hours()
-console.log(time)
 
 
 
+//displays the timesblocks
 var containerEl = document.querySelectorAll('#hour-task');
 
 for( i = 0; i < containerEl.length; i++)
 {
-    
-    var formEl = document.querySelector('#task-sec')
     var currentblock = parseInt(containerEl[i].getAttribute('data-time'))
     var currentData = localStorage.getItem(containerEl[i].getAttribute('data-time'))
-    // containerEl[i].appedendChild(currentData)
-    
-    formEl.innerHTML === currentData
+
+
+ //saves and returns local storage data to the user (start)
+
+    var currentText9 = document.querySelector('#task-sec9')
+    currentText9.addEventListener("keyup", event =>
+    {
+        localStorage.setItem("text9", event.target.value)
+    })
+    currentText9.value = localStorage.getItem("text9")
+
+
+    var currentText10 = document.querySelector('#task-sec10')
+    currentText10.addEventListener("keyup", event =>
+    {
+        localStorage.setItem("text10", event.target.value)
+    })
+    currentText10.value = localStorage.getItem("text10")
+
+    var currentText11 = document.querySelector('#task-sec11')
+    currentText11.addEventListener("keyup", event =>
+    {
+        localStorage.setItem("text11", event.target.value)
+    })
+    currentText11.value = localStorage.getItem("text11")
+
+
+    var currentText12 = document.querySelector('#task-sec12')
+    currentText12.addEventListener("keyup", event =>
+    {
+        localStorage.setItem("text12", event.target.value)
+    })
+    currentText12.value = localStorage.getItem("text12")
+
+
+    var currentText13 = document.querySelector('#task-sec13')
+    currentText13.addEventListener("keyup", event =>
+    {
+        localStorage.setItem("text13", event.target.value)
+    })
+    currentText13.value = localStorage.getItem("text13")
+
+
+    var currentText14 = document.querySelector('#task-sec14')
+    currentText14.addEventListener("keyup", event =>
+    {
+        localStorage.setItem("text14", event.target.value)
+    })
+    currentText14.value = localStorage.getItem("text14")
+
+
+    var currentText15 = document.querySelector('#task-sec15')
+    currentText15.addEventListener("keyup", event =>
+    {
+        localStorage.setItem("text15", event.target.value)
+    })
+    currentText15.value = localStorage.getItem("text15")
+
+
+    var currentText16 = document.querySelector('#task-sec16')
+    currentText16.addEventListener("keyup", event =>
+    {
+        localStorage.setItem("text16", event.target.value)
+    })
+    currentText16.value = localStorage.getItem("text16")
+
+
+    var currentText17 = document.querySelector('#task-sec17')
+    currentText17.addEventListener("keyup", event =>
+    {
+        localStorage.setItem("text17", event.target.value)
+    })
+    currentText17.value = localStorage.getItem("text17")
+
+     //saves and returns local storage data to the user (end)
+
+
+// checks time and changes the color depending if the hour is in the past present or future     
 
 if(currentblock < time)
 {
@@ -41,6 +107,7 @@ if(currentblock > time)
 }
 
 
+// array for dolgin the proper times and display times
 
          const timeObj = 
             {
@@ -54,15 +121,8 @@ if(currentblock > time)
                 "4 PM" : 16,
                 "5 PM" : 17
             }
-
-
 var displayTimes = function()
 {
-
-   
-
-        
-
     for(i = 0; i < timeObj.lenght; i++)
     {
        console.log(timeObj)
@@ -70,20 +130,16 @@ var displayTimes = function()
 
 }; 
 
+//save button click event 
 
 var savebtn =  $('.save-btn') 
 
 savebtn.on('click', function(event)
 {   
-    // containerEl.innerHTML = "";
-    event.preventDefault();
-    console.log('>>>event>>>', event.target);
+    event.preventDefault(); 
     var clickedBtn = $(this);
     var textArea = clickedBtn.prev().siblings();
     var timeDiv = textArea.prev()
-    console.log('>>> text area content >>>', timeDiv.val());
-    console.log('>>>> time div data time>>>', timeDiv.data("text"))
-    console.log(event.target)
     localStorage.setItem(timeDiv.data("text"), timeDiv.val())
 });
 
